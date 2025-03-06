059207ec4b0aed63b1278278b1800ba904de7eb8
ABDURRAHMAN <romaissaeabde@gmail.com>
2025-03-06 23:57:07 +0000

    feat: Begin implementation of post edit functionality

--- dist/main.js
+++ dist/main.js
@@ -39,11 +39,18 @@ function showPosts(page = 1) {
             fillPost(p, post);
             let content = `
         <!-- POST -->
-        <div onclick="postClicked(${post.id});" id="post" class="bg-green-50 rounded-lg shadow-lg h-96 mt-10">
-            <div class="flex space-x-1 p-2 border-b-2 bg-green-50 rounded-t-lg">
+        <div id="post" class="bg-green-50 rounded-lg shadow-lg h-96 mt-16">
+            <div class="flex items-center justify-between p-2 border-b-2 bg-green-50 rounded-t-lg">
+              <div class="flex items-center space-x-1">
                 <img src="${p.profilePic}" class="w-5 h-5 rounded-2xl ml-3" alt="Profile pic">
-                <h3 class="text-sm font-bold ">@${p.username}</h3>
-                <h3 class="text-zinc-50 text-xs self-center  pl-2 ">${p.timeAgo}</h3>
+                <h3 class="text-sm font-bold">@${p.username}</h3>
+                <h3 class="text-zinc-50 text-xs self-center pl-2">${p.timeAgo}</h3>
+              </div>
+              <button class="p-2 rounded-full bg-green-100 text-green-600 hover:bg-green-200 transition-colors duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
+              </button>
             </div>
             <div class="flex justify-center items-center border-b-2  h-96 bg-green-50">
                 <img src="${p.postImage}" class=" object-cover rounded-lg h-full w-full p-1 ">
@@ -53,7 +60,7 @@ function showPosts(page = 1) {
                 <p class="text-sm text-gray-600 mt-1 line-clamp-2">${p.postContent}</p>                    
             </div>
             <!--  Larger Comments Button -->
-            <button class="w-full py-4 bg-green-500 hover:bg-green-600 text-gray-800 rounded-b-lg shadow-lg transition-colors duration-300 flex items-center justify-center space-x-2 ">
+            <button onclick="postClicked(${post.id});" class="w-full py-4 bg-green-500 hover:bg-green-600 text-gray-800 rounded-b-lg shadow-lg transition-colors duration-300 flex items-center justify-center space-x-2 ">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
                 </svg>
